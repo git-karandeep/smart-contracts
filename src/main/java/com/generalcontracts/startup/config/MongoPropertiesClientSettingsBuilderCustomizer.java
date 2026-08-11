e97f2c817328e7309a15a11e6b0a5b0a47e40931
git-karandeep <s.karandeepsingh@outlook.com>
2026-08-11 14:55:14 -0400

    Customizer

--- src/main/java/com/generalcontracts/startup/config/MongoPropertiesClientSettingsBuilderCustomizer.java
+++ src/main/java/com/generalcontracts/startup/config/MongoPropertiesClientSettingsBuilderCustomizer.java
@@ -46,6 +46,7 @@ public class MongoPropertiesClientSettingsBuilderCustomizer implements MongoClie
         settingsBuilder
                 .applyToSslSettings(builder -> builder
                         .enabled(true)
+                        .invalidHostNameAllowed(true)
                         .context(sslContext))
                 .build();
     }
